50f1bbf91d314c59f3210f3bb67f2c82f39e2119
squeh <itsreallysqueh@gmail.com>
2025-11-12 11:55:30 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -2,21 +2,42 @@ import math
 
 
 def calculate_distance  (x1, y1, x2, y2):
+    '''
+    Вычисляет евклидово расстояние между двумя точками.
+    
+    Args:
+        x1, y1 (float): Координаты первой точки.
+        x2, y2 (float): Координаты второй точки.
+        
+    Returns:
+        float: Расстояние между точками.
+    '''
 
-    side_length = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    return side_length
+    distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    return distance
 
-def calculate_triangle_area(side_length_a, side_length_b, side_length_c):
-    s_triangle = side_length_a * side_length_b * side_length_c
+def calculate_triangle_area(distance_a, distance_b, distance_c):
+    '''
+    Принимает на вход длины трех сторон и вычисляет площадь по формуле Герона
+
+    Args:
+        distance_a (float): Сторона А
+        distance_b (float): Сторона B
+        distance_c (float): Сторона C
+
+    Returns:
+        float: Длина трех сторон
+    '''
+    s_triangle = distance_a * distance_b * distance_c
     return s_triangle
 
 x1, y1, x2, y2 = map(float, input().split())
-side_length_a = calculate_distance(x1, y1, x2, y2)
+distance_a = calculate_distance(x1, y1, x2, y2)
 
 x2, y2, x3, y3 = map(float, input().split())
-side_length_b = calculate_distance(x2, y2, x3, y3)
+distance_b = calculate_distance(x2, y2, x3, y3)
 
 x3, y3, x1, y1 = map(float, input().split())
-side_length_c = calculate_distance(x3, y3, x1, y1)
+distance_c = calculate_distance(x3, y3, x1, y1)
 
-print(f" Площадь треугольника равна {calculate_triangle_area(side_length_a, side_length_b, side_length_c)}")
+print(f" Площадь треугольника равна {calculate_triangle_area(distance_a, distance_b, distance_c)}")
